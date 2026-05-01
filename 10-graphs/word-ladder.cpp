7b5001532060bac0ed4ec844d8ecd7c5823e22a2
sudo-sanjeev <sanjeev.singh.hire@gmail.com>
2026-05-01 20:37:20 +0530

    word ladder

--- 10-graphs/word-ladder.cpp
+++ 10-graphs/word-ladder.cpp
@@ -1,3 +1,10 @@
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <queue>
+
+using namespace std;
+
 class Solution {
     public:
         int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
